fbb0fb1a7eff226c904e64146efa9e77cea12199
Nick Rycar <rycar@chef.io>
2015-07-09 22:29:13 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# total_dbag
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: total_dbag
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
